cdbfe1b0e30518a4bd0bc53f197502249b1fdbf7
mktsyy <mktsyy@gmail.com>
2018-01-02 16:30:45 +0800

    修改识别，根据牌上显示白色来进行牌判断，准备加入一次判断

--- cardrecognise/card/middlecard.py
+++ cardrecognise/card/middlecard.py
@@ -84,20 +84,20 @@ def cmpcolor(coordinate):
 
 def main():
 	##print win32gui.GetCursorPos()
-	im = ImageGrab.grab() 
-	im.save(os.getcwd()+"/screenshot.png")#保存图片 
-	##os.execvp( "mspaint",('mspaint','c:/screenshot.png'))#调用画图程序打开截屏图片
+	# im = ImageGrab.grab() 
+	# im.save(os.getcwd()+"/screenshot.png")#保存图片 
+	# ##os.execvp( "mspaint",('mspaint','c:/screenshot.png'))#调用画图程序打开截屏图片
 
 
-	im1=Image.open(os.getcwd()+"/screenshot.png")
-	# print im1.size
-	rec=(569,751,746,817)
-	region=im1.crop(rec)
-	# region.show()
-	region.save(os.getcwd()+"/123.bmp")
+	# im1=Image.open(os.getcwd()+"/screenshot.png")
+	# # print im1.size
+	# rec=(569,751,746,817)
+	# region=im1.crop(rec)
+	# # region.show()
+	# region.save(os.getcwd()+"/123.bmp")
 
 	img = Image.open('123.bmp')
-	# print img.getpixel((2,6))
+	print img.getpixel((31,29))
 
 	# ##用ipython调试
 	# from IPython import embed
@@ -106,169 +106,169 @@ def main():
 	# if cmpcolor((2,6)) or cmpcolor((2,12)) or cmpcolor((2,10)) or cmpcolor((2,14)):
 	
 
-	#数字7
+	# #数字7
 
 	
 
-	region = (86,1,108,30)
+	# region = (86,1,108,30)
 
-	cropImg = img.crop(region)
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middle7.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle7.jpg")
 
-	#数字A
-
-	
-
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
-
-	cropImg.save(os.getcwd()+"\\middletemp\\middleA.jpg")
-
-	#数字K
+	# #数字A
 
 	
 
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middleK.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middleA.jpg")
 
-	#数字4
-
-	
-
-	region = (84,1,110,31) 
-	cropImg = img.crop(region)
-
-	cropImg.save(os.getcwd()+"\\middletemp\\middle4.jpg")
-
-	#数字2
+	# #数字K
 
 	
 
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middle2.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middleK.jpg")
 
-	#数字3
-
-	
-
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
-
-	cropImg.save(os.getcwd()+"\\middletemp\\middle3.jpg")
-
-	#数字5
+	# #数字4
 
 	
 
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
+	# region = (84,1,110,31) 
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middle5.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle4.jpg")
 
-	#数字6
-
-	
-
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
-
-	cropImg.save(os.getcwd()+"\\middletemp\\middle6.jpg")
-
-	#数字8
+	# #数字2
 
 	
 
-	region = (86,0,108,30) 
-	cropImg = img.crop(region)
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middle8.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle2.jpg")
 
-	#数字9
-
-	
-
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
-
-	cropImg.save(os.getcwd()+"\\middletemp\\middle9.jpg")
-
-	#数字J
+	# #数字3
 
 	
 
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middleJ.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle3.jpg")
 
-	#数字Q
-
-	
-
-	region = (86,1,108,30) 
-	cropImg = img.crop(region)
-
-	cropImg.save(os.getcwd()+"\\middletemp\\middleQ.jpg")
-
-	#数字10
+	# #数字5
 
 	
 
-	region = (82,1,111,28) 
-	cropImg = img.crop(region)
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
 
-	cropImg.save(os.getcwd()+"\\middletemp\\middle10.jpg")
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle5.jpg")
 
-	################以下完成      
-	cutpic = os.listdir(os.getcwd()+"\\middletemp")
-	# a=getImgHash("temp.jpg")#图片地址自行替换 
-	comparelist = []
-	secondcard = []
-	lastlist = []
-	for pic in cutpic:
-		a=getImgHash(os.getcwd()+"\\middletemp\\"+pic)#图片地址自行替换 
-		files = os.listdir(os.getcwd()+"\\num")#图片文件夹地址自行替换  
+	# #数字6
 
-		#用ipython调试
-		# from IPython import embed
-		# embed()
+	
 
-		for file in files:
-		   # print file
-			if not file in"recognize.pycutpic.pyNpicrecognize.pycutpicsendcard.pycutpicfirstcard.pytempall.pynumtempleftcard.py":
-				b=getImgHash(os.getcwd()+"\\num\\"+file)  
-				compare=getMH(a,b)  
-				comparelist.append(int(compare)) 
-				strdata = str(pic)+"............"+ file+u'相似度'+str(compare)+'%'
-				secondcard.append(strdata)
-				# print str(pic)+"............"+ file,u'相似度',str(compare)+'%'
-		# if int(max(comparelist))< 75:
-		# 	print u"没有识别"
-		# else:
-		# 	print str(pic)+"............"+ file,u'最相似,相似度',str(max(comparelist))+'%'
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
 
-	##用ipython调试
-	## from IPython import embed
-	## embed()	
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle6.jpg")
 
-	for ii in secondcard:
-		if str(max(comparelist)) in ii:
-			# print('\033[1;33;40m')
-			if int(max(comparelist))> 80:
-				# print (u"PLAYER当中位......."+ii.split(".bmp")[0].split("............")[1])
-				if "-" in ii:
-					ii = ii.replace("-1",' ')
-				lastlist.append(u"PLAYER当中位......."+ii.split(".bmp")[0].split("............")[1])
-			# else:
-			# 	raise RuntimeError
+	# #数字8
 
-	if (len(lastlist)>=1):
-		print lastlist[0]
+	
+
+	# region = (86,0,108,30) 
+	# cropImg = img.crop(region)
+
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle8.jpg")
+
+	# #数字9
+
+	
+
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
+
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle9.jpg")
+
+	# #数字J
+
+	
+
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
+
+	# cropImg.save(os.getcwd()+"\\middletemp\\middleJ.jpg")
+
+	# #数字Q
+
+	
+
+	# region = (86,1,108,30) 
+	# cropImg = img.crop(region)
+
+	# cropImg.save(os.getcwd()+"\\middletemp\\middleQ.jpg")
+
+	# #数字10
+
+	
+
+	# region = (82,1,111,28) 
+	# cropImg = img.crop(region)
+
+	# cropImg.save(os.getcwd()+"\\middletemp\\middle10.jpg")
+
+	# ################以下完成      
+	# cutpic = os.listdir(os.getcwd()+"\\middletemp")
+	# # a=getImgHash("temp.jpg")#图片地址自行替换 
+	# comparelist = []
+	# secondcard = []
+	# lastlist = []
+	# for pic in cutpic:
+	# 	a=getImgHash(os.getcwd()+"\\middletemp\\"+pic)#图片地址自行替换 
+	# 	files = os.listdir(os.getcwd()+"\\num")#图片文件夹地址自行替换  
+
+	# 	#用ipython调试
+	# 	# from IPython import embed
+	# 	# embed()
+
+	# 	for file in files:
+	# 	   # print file
+	# 		if not file in"recognize.pycutpic.pyNpicrecognize.pycutpicsendcard.pycutpicfirstcard.pytempall.pynumtempleftcard.py":
+	# 			b=getImgHash(os.getcwd()+"\\num\\"+file)  
+	# 			compare=getMH(a,b)  
+	# 			comparelist.append(int(compare)) 
+	# 			strdata = str(pic)+"............"+ file+u'相似度'+str(compare)+'%'
+	# 			secondcard.append(strdata)
+	# 			# print str(pic)+"............"+ file,u'相似度',str(compare)+'%'
+	# 	# if int(max(comparelist))< 75:
+	# 	# 	print u"没有识别"
+	# 	# else:
+	# 	# 	print str(pic)+"............"+ file,u'最相似,相似度',str(max(comparelist))+'%'
+
+	# ##用ipython调试
+	# ## from IPython import embed
+	# ## embed()	
+
+	# for ii in secondcard:
+	# 	if str(max(comparelist)) in ii:
+	# 		# print('\033[1;33;40m')
+	# 		if int(max(comparelist))> 80:
+	# 			# print (u"PLAYER当中位......."+ii.split(".bmp")[0].split("............")[1])
+	# 			if "-" in ii:
+	# 				ii = ii.replace("-1",' ')
+	# 			lastlist.append(u"PLAYER当中位......."+ii.split(".bmp")[0].split("............")[1])
+	# 		# else:
+	# 		# 	raise RuntimeError
+
+	# if (len(lastlist)>=1):
+	# 	print lastlist[0]
 
 if __name__ == '__main__':
 	# main()
--- recognizepic/remain.py
+++ recognizepic/remain.py
@@ -10,23 +10,59 @@ from pytesser import *
 from recognizenum import *
 import time
 
+
+# ifcolor = [(233, 241, 243),(237, 238, 240),(238, 240, 239),(235, 237, 236),(236, 238, 237),
+#           (237, 238, 233),(242, 239, 232),(248, 233, 228),(235, 239, 238),(238, 238, 238),
+#           (233, 239, 239),(237, 237, 237),(241, 236, 232),(255, 231, 237),(235, 241, 237),
+#           (234, 240, 238),(237, 238, 243),(251, 230, 235),(231, 241, 242),
+#           (52, 111, 119),(243, 238, 234),(231, 243, 239),(238, 239, 243),(227, 244, 238),
+#           (232, 241, 246),(236, 239, 244),(231, 243, 239),(245, 235, 233),(250, 229, 234),
+#           (231, 243, 241), (221, 244, 250), (235, 240, 244), (231, 243, 241), (239, 238, 243),
+#           (242, 237, 233), (243, 236, 226),(243, 238, 232),(234, 239, 242),(230, 241, 247),
+#           (64, 100, 90), (237, 239, 238), (240, 237, 232),(236, 240, 241),
+#           (245, 234, 228), (245, 237, 234), (243, 236, 228),(248, 233, 226),(232, 241, 240),
+#           (247, 232, 225),(239, 236, 231),(239, 237, 240),(252, 234, 234),(243, 238, 219),
+# 		(231, 231, 229),(236, 239, 230),(241, 226, 229),(252, 238, 235),
+#           (238, 242, 241), (235, 239, 240), (246, 233, 225), (240, 237, 232),
+#           (247, 234, 228), (243, 240, 221),(240, 237, 244),(247, 232, 235),
+
+#           (235, 240, 243), (238, 239, 244), (243, 234, 227), (247, 236, 234),
+#           (245, 245, 243), (235, 241, 241), (238, 237, 242), (238, 238, 240),
+#            ]
+
+ifcolor = []
+for i in xrange(210,255):
+	for n in xrange(210,255):
+		for g in xrange(210,255):
+			ifcolor.append((i,n,g))
+
+
+
 while True:
-	time.sleep(0.2)
+	time.sleep(1)
 
 	##PLAYER
+
 	cutpic((569,751,746,817),"testnew.jpg")
+
 	img = Image.open("testnew.jpg")
-	cutnum(img,(86,1,108,30),'PLAYER-middle.jpg')
-	print u"PLAYER-当中是。。。。。"+str(getverify1('PLAYER-middle.jpg'))  #middlecard
+	# print img.getpixel((79,16))
+	if img.getpixel((79,16)) in ifcolor:
+		cutnum(img,(86,1,111,30),'PLAYER-middle.jpg')
+		print u"PLAYER-当中是。。。。。"+str(getverify1('PLAYER-middle.jpg'))  #middlecard
 
-	cutnum(img,((140,0,165,31)),'PLAYER-right.jpg')
-	print u"PLAYER-右边是。。。。。"+str(getverify1('PLAYER-right.jpg'))  #rightcard
+	# print img.getpixel((134,16))
+	if img.getpixel((134,16)) in ifcolor:
+		cutnum(img,((138,0,167,31)),'PLAYER-right.jpg')
+		print u"PLAYER-右边是。。。。。"+str(getverify1('PLAYER-right.jpg'))  #rightcard
 
-	cutnum(img,((0,30,31,53)),'PLAYER-left.jpg')
-	leftpic = Image.open('PLAYER-left.jpg')
-	newLP = leftpic.rotate(270)
-	newLP.save("PLAYER-left.jpg")
-	print u"PLAYER-左边是。。。。。"+str(getverify1('PLAYER-left.jpg'))  #leftcard
+	# print img.getpixel((23,23))
+	if img.getpixel((23,23)) in ifcolor:
+		cutnum(img,((0,30,34,53)),'PLAYER-left.jpg')
+		leftpic = Image.open('PLAYER-left.jpg')
+		newLP = leftpic.rotate(270)
+		newLP.save("PLAYER-left.jpg")
+		print u"PLAYER-左边是。。。。。"+str(getverify1('PLAYER-left.jpg'))  #leftcard
 
 	##BANKER
 
@@ -77,17 +113,27 @@ while True:
 
 
 	clr = Color()
+
 	cutpic((1173,751,1350,817),"BANKER.jpg")
+
 	img = Image.open("BANKER.jpg")
-	cutnum(img,(69,1,91,30),'BANKER-middle.jpg')
-	clr.print_blue_text(u"BANKER-当中是。。。。。"+str(getverify1('BANKER-middle.jpg')))  #middlecard
+	
+	# print img.getpixel((62,26))
+	if img.getpixel((62,26)) in ifcolor:
+		cutnum(img,(69,1,92,32),'BANKER-middle.jpg')
+		clr.print_blue_text(u"BANKER-当中是。。。。。"+str(getverify1('BANKER-middle.jpg')))  #middlecard
 
-	cutnum(img,((112,30,144,54)),'BANKER-right.jpg')
-	rightpic = Image.open('BANKER-right.jpg')
-	newRP = rightpic.rotate(270)
-	newRP.save("BANKER-right.jpg")
-	clr.print_blue_text(u"BANKER-右边是。。。。。"+str(getverify1('BANKER-right.jpg')))  #rightcard
+	# print img.getpixel((7,26))
+	if img.getpixel((7,26)) in ifcolor:
+		cutnum(img,((12,1,39,32)),'BANKER-left.jpg')
+		clr.print_blue_text(u"BANKER-左边是。。。。。"+str(getverify1('BANKER-left.jpg')))  #leftcard
 
-	cutnum(img,((12,1,37,32)),'BANKER-left.jpg')
-	clr.print_blue_text(u"BANKER-左边是。。。。。"+str(getverify1('BANKER-left.jpg')))  #leftcard
+	# print img.getpixel((137,26))
+	if img.getpixel((137,26)) in ifcolor:
+		cutnum(img,((112,30,144,54)),'BANKER-right.jpg')
+		rightpic = Image.open('BANKER-right.jpg')
+		newRP = rightpic.rotate(270)
+		newRP.save("BANKER-right.jpg")
+		clr.print_blue_text(u"BANKER-右边是。。。。。"+str(getverify1('BANKER-right.jpg')))  #rightcard
+
 
