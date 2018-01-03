df4501ad18f1e1c754a4438b1d620b4f1763eb1c
mktsyy <mktsyy@gmail.com>
2018-01-03 16:59:55 +0800

    1加test4.dll对应的c代码图 2加入简单注释

--- import_C/import_C.py
+++ import_C/import_C.py
@@ -14,14 +14,15 @@ from ctypes import *
 # print pDll.sum(a)
 
 ##调用C函数结构
+
+##针对c写出对应的结构类
 class testdll(Structure):  
     _fields_=[('total',c_int),  
              ('avg',c_double)]  
-t=testdll()  
-t.total=10  
-t.avg=0.2 
 
 pDll=WinDLL("test4.dll")
+
+##其中sum是c的函数名称
 pDll.sum.restype=testdll 
 ARRAY = (c_int *13) #包含4个整数的数组类型
 a = ARRAY(0,1,2,3,4,5,6,7,8,9,0,11,12) #声明包含4个整数的数组，初值均为0     
