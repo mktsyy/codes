9383b64cb28057d4407a51b1b166071fc1f64e8b
mktsyy <mktsyy@gmail.com>
2018-01-08 10:17:00 +0800

    import_c test5.dll

--- import_C/import_C.py
+++ import_C/import_C.py
@@ -22,12 +22,13 @@ class testdll(Structure):
     _fields_=[('total',c_int),  
              ('avg',c_double)]  
 
-pDll=WinDLL("test4.dll")
+pDll=WinDLL("test5.dll")
 
 ##其中sum是c的函数名称
-pDll.sum.restype=testdll 
-ARRAY = (c_int *13) #包含4个整数的数组类型
-a = ARRAY(0,1,2,3,4,5,6,7,8,9,0,11,12) #声明包含4个整数的数组，初值均为0     
-t=pDll.sum(a)  
-print t.total  
-print t.avg
+pDll.Dec_bet.restype=testdll 
+ARRAY = (c_int *14) #包含4个整数的数组类型
+# a = ARRAY(0,1,2,3,4,5,6,7,8,9,0,11,12,13) #声明包含4个整数的数组，初值均为0     
+a = ARRAY(0,1,2,3,3,3,3,3,3,3,3,3,3,3)
+t=pDll.Dec_bet(a)  
+print t.Banker_wager  
+print Player_wager
