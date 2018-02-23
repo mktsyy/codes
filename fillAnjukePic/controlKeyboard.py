df4c3d1b5c3a711342657c9c725d043adb884d67
mktsyy <mktsyy@gmail.com>
2018-02-24 00:26:27 +0800

    写了个加载安居客室内图可带参数运行的小插件

---
+++ fillAnjukePic/controlKeyboard.py
@@ -0,0 +1,25 @@
+##controlKeyboard
+#-*-coding:utf-8-*-
+##这个是关于1920*1080分辨率的
+
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+def doKeyboard():
+	with keyboard.pressed(Key.ctrl):
+		keyboard.press('a')
+		keyboard.release('a')
+
+def altUp():
+	with keyboard.pressed(Key.alt):
+		keyboard.press(Key.up)
+		keyboard.release(Key.up)
+
+def down():
+	keyboard.press(Key.down)
+	keyboard.release(Key.down)
+
+def enter():
+	keyboard.press(Key.enter)
+	keyboard.release(Key.enter)
